650e007b2daaba9a0a48706addbdfa474c9f6bc9
unknown <kristhian.segura@labinfo.is.escuelaing.edu.co>
2022-02-28 11:00:39 -0500

    Cambios POM

--- src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
+++ src/main/java/edu/eci/arsw/blueprints/controllers/BlueprintAPIController.java
@@ -7,7 +7,18 @@ package edu.eci.arsw.blueprints.controllers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import edu.eci.arsw.model.Blueprint;
+import edu.eci.arsw.services.BluePrintsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +27,24 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/blueprints")
 public class BlueprintAPIController {
-    
-    
-    
-    
-    
+
+    @Autowired
+    BluePrintsServices bps;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecurso() {
+        try {
+            Set<Blueprint> data = bps.getAllBlueprints();
+            return new ResponseEntity<>(data, HttpStatus.ACCEPTED);
+        } catch (Exception ex) {
+            Logger.getLogger(BlueprintAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error bla bla bla", HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 }
 
--- src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
+++ src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"edu.eci.arsw.blueprints"})
+@ComponentScan(basePackages = {"edu.eci.arsw.*"})
 public class BlueprintsAPIApplication {
 
 	public static void main(String[] args) {
